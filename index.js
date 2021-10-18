99bf880ecc9f288ceb83dfa837801fa39791034f
dialloflatno <brahimdiallo23@gmail.com>
2021-10-18 18:35:30 -0400

    commit tree b415609a1f5ad702a326e50fca159a6ef7dedf77

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",function(){
+    console.log(" After DOM is loaded")
+   phrase = document.getElementById('text').textContent ="This is really cool!"
+})
+
+
+    
+
+console.log("Before is loaded");
